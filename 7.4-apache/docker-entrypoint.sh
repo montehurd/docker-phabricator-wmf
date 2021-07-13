299a46a3474da3e24122f02166f8fa3cbff2ac3c
montehurd <mhurd@wikimedia.org>
2021-07-13 12:05:38 -0700

    update more settings

--- 7.4-apache/docker-entrypoint.sh
+++ 7.4-apache/docker-entrypoint.sh
@@ -2,21 +2,10 @@
 
 set -e
 
-MYSQL_HOST=${MYSQL_HOST:-localhost}
-MYSQL_PORT=${MYSQL_PORT:-3306}
-MYSQL_USER=${MYSQL_USER:-phabricator}
-MYSQL_PASS=${MYSQL_PASS:-phabricator}
-
-export PHABRICATOR_HOST=${PHABRICATOR_HOST:-example.jp}
-export PHABRICATOR_BASE_URI=${PHABRICATOR_BASE_URI:-http://$PHABRICATOR_HOST}
-export PHABRICATOR_REPO_LOCAL_PATH=${PHABRICATOR_REPO_LOCAL_PATH:-/var/repo}
-export PHABRICATOR_LOG_HOME=${PHABRICATOR_LOG_HOME:-/var/log/phabricator}
-export PHABRICATOR_LOG_ACCESS_PATH=${PHABRICATOR_LOG_ACCESS_PATH:-$PHABRICATOR_LOG_HOME/access.log}
-export PHABRICATOR_LOG_SSH_PATH=${PHABRICATOR_LOG_SSH_PATH:-$PHABRICATOR_LOG_HOME/ssh.log}
-export PHABRICATOR_LOG_PHD_HOME=${PHABRICATOR_LOG_PHD_HOME:-/var/tmp/phd/log}
-
 # [ "$1" = "/usr/bin/supervisord" ] || exec "$@" || exit $?
 [ "$1" = "apache2-foreground" ] || exec "$@" || exit $?
+
+sleep 60;
 
 while ! nc -vz ${MYSQL_HOST} ${MYSQL_PORT};
 do
@@ -27,6 +16,7 @@ done;
 echo MySQL Connected!;
 
 echo "Start configuration..."
+
 ./bin/config set mysql.host $MYSQL_HOST
 ./bin/config set mysql.port $MYSQL_PORT
 ./bin/config set mysql.user $MYSQL_USER
@@ -37,16 +27,38 @@ echo "Start configuration..."
 ./bin/config set log.ssh.path "$PHABRICATOR_LOG_SSH_PATH"
 ./bin/config set phd.log-directory "$PHABRICATOR_LOG_PHD_HOME"
 ./bin/config set phabricator.base-uri $PHABRICATOR_BASE_URI
+./bin/config set metamta.default-address $PHABRICATOR_METAMTA_DEFAULT_ADDRESS
 ./bin/config set storage.mysql-engine.max-size 8388608
+./bin/config set phabricator.developer-mode true
+./bin/config set user.require-real-name false
 
-[ -v PABRICATOR_CLUSTER_MAILERS_JSON ] && ./bin/config set cluster.mailers "$PABRICATOR_CLUSTER_MAILERS_JSON"
-[ -v PABRICATOR_METAMTA_DEFAULT_ADDRESS ] && ./bin/config set metamta.default-address $PABRICATOR_METAMTA_DEFAULT_ADDRESS
+# cat > conf/local/local.json << EOL
+# {
+#   "user.require-real-name": false,
+#   "phabricator.developer-mode": true,
+#   "storage.mysql-engine.max-size": 8388608,
+#   "metamta.default-address": "$PHABRICATOR_METAMTA_DEFAULT_ADDRESS",
+#   "phabricator.base-uri": "$PHABRICATOR_BASE_URI",
+#   "phd.log-directory": "$PHABRICATOR_LOG_PHD_HOME",
+#   "log.ssh.path": "$PHABRICATOR_LOG_SSH_PATH",
+#   "log.access.path": "$PHABRICATOR_LOG_ACCESS_PATH",
+#   "repository.default-local-path": "$PHABRICATOR_REPO_LOCAL_PATH",
+#   "pygments.enabled": true,
+#   "mysql.pass": "$MYSQL_PASS",
+#   "mysql.user": "$MYSQL_USER",
+#   "mysql.port": "$MYSQL_PORT",
+#   "mysql.host": "$MYSQL_HOST"
+# }
+# EOL
 
-if [ -v PHABRICATOR_STORAGE_LOCAL_PATH ]; then
-	./bin/config set storage.local-disk.path "$PHABRICATOR_STORAGE_LOCAL_PATH"
-	mkdir -p "$PHABRICATOR_STORAGE_LOCAL_PATH"
-	chown -R www-data:www-data "$PHABRICATOR_STORAGE_LOCAL_PATH"
-fi
+# [ -v PHABRICATOR_CLUSTER_MAILERS_JSON ] && ./bin/config set cluster.mailers "$PHABRICATOR_CLUSTER_MAILERS_JSON"
+# [ -v PHABRICATOR_METAMTA_DEFAULT_ADDRESS ] && ./bin/config set metamta.default-address $PHABRICATOR_METAMTA_DEFAULT_ADDRESS
+# 
+# if [ -v PHABRICATOR_STORAGE_LOCAL_PATH ]; then
+# 	./bin/config set storage.local-disk.path "$PHABRICATOR_STORAGE_LOCAL_PATH"
+# 	mkdir -p "$PHABRICATOR_STORAGE_LOCAL_PATH"
+# 	chown -R www-data:www-data "$PHABRICATOR_STORAGE_LOCAL_PATH"
+# fi
 
 mkdir -p "$PHABRICATOR_REPO_LOCAL_PATH"
 chown -R www-data:www-data "$PHABRICATOR_REPO_LOCAL_PATH"
